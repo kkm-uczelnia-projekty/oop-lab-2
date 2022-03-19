90481aabe2c82c5a564fd332e208068b585aac9f
Jakub Surdej <kubasurdej@gmail.com>
2022-03-19 18:19:18 +0100

    Initialize project

---
+++ Person.cs
@@ -0,0 +1,28 @@
+namespace po
+{
+    class Person
+    {
+        protected string name { get; }
+        protected int age { get; }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public bool Equals(Person? otherPerson)
+        {
+            if (otherPerson == null) {
+                return false;
+            }
+
+            return this.age == otherPerson.age && this.name == otherPerson.name;
+        }
+
+        override public string ToString()
+        {
+            return String.Format("{0} ({1} y. o.)", name, age);
+        }
+    }    
+}
